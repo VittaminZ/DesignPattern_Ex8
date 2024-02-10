bd7926cccb1f68a92cbed63a26726f27b29524d3
i'm okay <98306512+VittaminZ@users.noreply.github.com>
2024-02-11 01:34:29 +0700

    Add files via upload

---
+++ 8.1/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        ImageManager.process(new SmartphoneRenderer());
+        // ImageManager.process(new LaptopRenderer());
+        // ImageManager.process(new TVRenderer());
+        //
+    }
+}
